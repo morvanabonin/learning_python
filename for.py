c5316f270baaf0c5f1b2b82f4de76991c7f30ba5
morvanabonin <morvanabonin-deathnote>
2015-11-26 23:44:51 -0200

    ...

---
+++ for.py
@@ -0,0 +1,16 @@
+impostos = ['MEI', 'Simples']
+for imposto in impostos:
+	print(imposto)
+
+for imposto in impostos:
+	if imposto.startswith("S"):
+		continue
+	print(imposto)
+
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in lista:
+	print(i)
+
+for i in range(5):
+	print(i)
+
